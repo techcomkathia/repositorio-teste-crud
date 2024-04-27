2f9b397586a6995705f24037971131888406ec4a
unknown <techcomk@gmail.com>
2024-04-27 18:45:52 -0300

    criação dos módulos e arquivo sql lpara criação do banco

--- telaCrud.py
+++ telaCrud.py
@@ -1,6 +1,27 @@
-from CRUD import *
 import streamlit as st
 
-# cadastrar('rapadura', 100, 88)
+st.title('Meu título')
+st.write('Olá mundão!')
 
-st.write('produtinho cadastrado')
+escolha = st.selectbox('Selecione o turno de interesse',
+                       ['Manha', 'tarde', 'noite'])
+
+# textos renderizados
+
+if escolha:
+    st.write(escolha)
+
+radio= st.radio('Selecione uma opção', ['Manha', 'tarde', 'noite'])
+
+texto = st.text_input('Digite seu nome')
+numero = st.number_input('Digite um numero')
+if texto and numero:
+    st.write(texto, numero)
+
+cpf = st.text_input("Digite seu CPF", placeholder="000.000.000-00")
+
+btnCadastrar = st.button('Cadastrar')
+
+st.write(btnCadastrar)
+
+textolongo = st.text_area('Digite sua reclamação')
