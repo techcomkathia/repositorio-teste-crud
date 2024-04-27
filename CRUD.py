82d752511b7c79996b93a0ecdaa56d7e81754299
Tech com Káthia <125410101+techcomkathia@users.noreply.github.com>
2024-04-27 10:31:28 -0300

    criação do modulo crud.py

---
+++ CRUD.py
@@ -0,0 +1,46 @@
+import mysql.connector
+
+
+
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="km2015km",
+    database="loja",
+)
+
+cursor = conexao.cursor()
+print("Conexão como o banco de dados feita com sucesso! \n")
+
+
+# funcao inserir
+def cadastrar ( nome, preco, id) :
+       comando_sql = f"insert into produtos (nome, preco, id) value ('{nome}', {preco},{id})"
+
+       cursor.execute(comando_sql)
+       conexao.commit()  # aqui é onde vamos inserir os dados
+       print("Produto cadastrado com sucesso!")
+
+
+
+#funcao selecionar
+def selecionarTodosProdutos():
+    comando_sql = f'select id, nome, preco from produtos'
+    cursor.execute(comando_sql)
+    resultado_consulta = cursor.fetchall()
+    return resultado_consulta
+
+
+#funcao atualizar
+def atualizarPreco(id, novo_valor):
+    comando_sql = f' UPDATE produtos SET preco = {novo_valor} WHERE id = {id}'
+    cursor.execute(comando_sql)
+    conexao.commit()
+
+#funcao deletar
+def deletarProduto(id):
+   comando_sql = f'DELETE FROM produtos WHERE id = {id}'
+   cursor.execute(comando_sql)
+   conexao.commit()
+
---
+++ telaCrud.py
@@ -0,0 +1,6 @@
+from CRUD import *
+import streamlit as st
+
+# cadastrar('rapadura', 100, 88)
+
+st.write('produtinho cadastrado')
